690d0d825ea42ea203c0b0e1bc00b111a42c919e
Hasan Zahid <guszahidha@student.gu.se>
2025-07-16 21:26:40 +0600

    #14 fix the apostrophe issue in line 28 in hero.tsx

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -20,11 +20,10 @@ export const Hero: React.FC<HeroProps> = ({ name, title }) => {
     <main className="container mx-auto py-8 px-4 md:px-8">
       <section
         id="hero"
-        className="text-center py-12 md:py-20 min-h-[calc(100vh-150px)] flex flex-col justify-center items-center"
-      >
+        className="text-center py-12 md:py-20 min-h-[calc(100vh-150px)] flex flex-col justify-center items-center">
         <div>
           <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold mb-4">
-            Hi, I'm{" "}
+            Hi, I’m{" "}
             <span className="text-sky-600 dark:text-sky-400">{name}</span>
           </h2>
           <p className="text-lg md:text-xl text-slate-700 dark:text-slate-300 mb-8 max-w-2xl mx-auto">
